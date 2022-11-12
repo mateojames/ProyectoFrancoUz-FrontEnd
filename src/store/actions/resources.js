fb42f1a4dc2f9c50c6bbbbb31fa92e2f8963ae23
bartom21 <60236508+bartom21@users.noreply.github.com>
2022-11-12 01:11:47 -0300

    url updated

--- src/store/actions/resources.js
+++ src/store/actions/resources.js
@@ -35,7 +35,7 @@ export const loadLocations = () => {
     return (dispatch, getState) => {
         getState().auth.currentUser.getIdToken(true)
             .then(idToken => {
-                fetch('http://localhost:8080/locations', {
+                fetch('https://back-red-team.vercel.app/locations', {
                     method: 'GET',
                     headers: {
                     "Content-Type": "application/json",
